c1bf35d8259d38ebaf5b943f190059181447ed1a
Asad Malik <asad@ubiquifydigital.com>
2023-05-26 23:20:45 -0500

    Adding rabbitmq, publishing msg from alpha vantage to queue

--- market-sources/alpha_vantage_sentiment.py
+++ market-sources/alpha_vantage_sentiment.py
@@ -1,14 +1,23 @@
 import os
+import json
+import pika
 import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
-API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY")
-assert API_KEY is not None
+API_KEY = os.environ["ALPHA_VANTAGE_API_KEY"]
+RABBITMQ_QUEUE = os.environ["RABBITMQ_QUEUE"]
+RABBITMQ_EXCHANGE = os.environ["RABBITMQ_EXCHANGE"]
+
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
 
 url = f"https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=AAPL&apikey={API_KEY}"
 r = requests.get(url)
 data = r.json()
+print(data)
+channel.basic_publish(exchange=RABBITMQ_EXCHANGE, routing_key=RABBITMQ_QUEUE, body=json.dumps(data))
 
 print(data)
--- rabbitmq-startup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -d --hostname rmq --name rabbit-server --platform=linux/arm/v7 -p 8080:15672 -p 5672:5672 rabbitmq:3-management
